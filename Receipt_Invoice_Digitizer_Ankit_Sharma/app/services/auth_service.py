b61310e14449bc03c7d0c894f8c66fc4de6931b7
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2026-03-08 16:58:43 +0530

    Initial commit

---
+++ Receipt_Invoice_Digitizer_Ankit_Sharma/app/services/auth_service.py
@@ -0,0 +1,40 @@
+from app.core.security import hash_password, verify_password, create_access_token
+from app.repositories.user_repo import get_user_by_email, create_user
+from app.models import User
+
+# checking fuction for user registered or not
+def register_user(db, data):
+    if get_user_by_email(db, data.user_email):
+        return None
+
+    # Check if first user
+    user_count = db.query(User).count()
+    role = "admin" if user_count == 0 else "user"
+
+    user = User(
+        user_name=data.user_name,
+        user_email=data.user_email,
+        user_password=hash_password(data.user_password),
+        role=role  # ← IMPORTANT
+    )
+
+    return create_user(db, user)
+
+# login function
+def login_user(db, data):
+    user = get_user_by_email(db, data.user_email)
+
+    if not user:
+        return None, None
+
+    if not verify_password(data.user_password, user.user_password):
+        return None, None
+
+    token = create_access_token(
+        {
+            "sub": str(user.user_id),
+            "role": user.role  
+        }
+    )
+
+    return user, token
---
+++ receipt_and_invoice_digitizer/src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Features from "./pages/Features";
+
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import AdminDashboard from "./pages/AdminDashboard";
+import "./App.css";
+import Auth from "./pages/auth/Auth";
+
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Auth />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/register" element={<Auth />} />
+
+        {/* Protected routes */}
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+        <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

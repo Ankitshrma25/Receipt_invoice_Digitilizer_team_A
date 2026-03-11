63be85ee57c98ca63b7b70a9b0281895c80157f9
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2026-03-11 16:03:55 +0530

    UI changes - Ankit Sharma

--- Receipt_Invoice_Digitizer_Ankit_Sharma/app/services/auth_service.py
+++ Receipt_Invoice_Digitizer_Ankit_Sharma/app/services/auth_service.py
@@ -1,13 +1,56 @@
+# from app.core.security import hash_password, verify_password, create_access_token
+# from app.repositories.user_repo import get_user_by_email, create_user
+# from app.models import User
+
+# # checking fuction for user registered or not
+# def register_user(db, data):
+#     if get_user_by_email(db, data.user_email):
+#         return None
+
+#     # Check if first user
+#     user_count = db.query(User).count()
+#     role = "admin" if user_count == 0 else "user"
+
+#     user = User(
+#         user_name=data.user_name,
+#         user_email=data.user_email,
+#         user_password=hash_password(data.user_password),
+#         role=role
+#     )
+
+#     return create_user(db, user)
+
+# # login function
+# def login_user(db, data):
+#     user = get_user_by_email(db, data.user_email)
+
+#     if not user:
+#         return None, None
+
+#     if not verify_password(data.user_password, user.user_password):
+#         return None, None
+
+#     token = create_access_token(
+#         {
+#             "sub": str(user.user_id),
+#             "role": user.role  
+#         }
+#     )
+
+#     return user, token
+
 from app.core.security import hash_password, verify_password, create_access_token
 from app.repositories.user_repo import get_user_by_email, create_user
 from app.models import User
 
-# checking fuction for user registered or not
+
+# Register user
 def register_user(db, data):
+    # Check if email already exists
     if get_user_by_email(db, data.user_email):
         return None
 
-    # Check if first user
+    # First registered user becomes admin
     user_count = db.query(User).count()
     role = "admin" if user_count == 0 else "user"
 
@@ -20,7 +63,8 @@ def register_user(db, data):
 
     return create_user(db, user)
 
-# login function
+
+# Login user
 def login_user(db, data):
     user = get_user_by_email(db, data.user_email)
 
@@ -30,10 +74,11 @@ def login_user(db, data):
     if not verify_password(data.user_password, user.user_password):
         return None, None
 
+    # Create JWT token containing user_id
     token = create_access_token(
         {
-            "sub": str(user.user_id),
-            "role": user.role  
+            "user_id": user.user_id,
+            "role": user.role
         }
     )
 
